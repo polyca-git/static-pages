a7f8cba6377c77ee8b071be5e84e141a8b0486f7
polyca-git <faruklittle@yandex.com>
2021-08-29 20:36:18 +0300

    Basic Functionalities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import {useState, useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      content
-    </div>
-  );
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(null);
+  const [items, setItems] = useState([]);
+  const [site, setSite] = useState([]);
+
+  useEffect(()=>{
+    fetch('./data/content.json')
+    .then(res => res.json())
+    .then(
+      (result)=>{
+        setIsLoaded(true);
+        console.log(result.posts);
+        setItems(result.posts);
+      },
+      (error)=> {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+  },[]);
+
+  useEffect(()=>{
+    fetch('./data/config.json')
+    .then(res => res.json())
+    .then(
+      (result)=>{
+        setSite(result.website);
+        document.title = result.website.title;
+      }
+    )
+  },[]);
+
+  if(error){
+    return <div>Error: {error.message}</div>;
+  } else if(!isLoaded){
+    return <div><div className="header"></div><div className="loading">Loading...</div></div>;
+  }else{
+    return (
+      <div className="App">
+        content
+      </div>
+    );
+  }
 }
 
 export default App;
